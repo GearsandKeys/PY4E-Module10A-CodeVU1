aeba21f8fe3eb495ba6b25fb3acef08f75d332c5
Nathan Gearke <Nathan.Gearke@veteransunited.com>
2022-04-08 09:26:53 -0500

    Added strip to get around autgrader input bug

--- possible_rates_chart.py
+++ possible_rates_chart.py
@@ -1,4 +1,8 @@
 def print_rates_chart() -> None:
     file_name = "pricing_sheet.csv"
 
+    #The strip() method is here because we need to strip the carriage return off the autograder's inputs
+    product_name = input("Enter Product Name: ").strip()
+    lock_period = input("Enter Lock Period : ").strip()
+
 print_rates_chart()
