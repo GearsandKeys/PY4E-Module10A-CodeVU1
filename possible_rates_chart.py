19bb322e5100016dfa4895a4ae834c8e3803d73e
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 15:12:41 -0600

    Update possible_rates_chart.py

--- possible_rates_chart.py
+++ possible_rates_chart.py
@@ -1,8 +1,7 @@
 def print_rates_chart() -> None:
     file_name = "pricing_sheet.csv"
+    
+    
 
-    #The strip() method is here because we need to strip the carriage return off the autograder's inputs
-    product_name = input("Enter Product Name: ").strip()
-    lock_period = input("Enter Lock Period : ").strip()
-
-print_rates_chart()
+if __name__ == "__main__":
+    print_rates_chart()
